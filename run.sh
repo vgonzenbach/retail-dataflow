80ed32c700d3a73b10524c477fb0dc4c9a59c355
Virgilio Gonzenbach <62161049+vgonzenbach@users.noreply.github.com>
2025-11-27 19:20:21 +0100

    add run wrapper

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export PROJECT_ID=gymshark-479509
+export TOPIC_ID=projects/$PROJECT_ID/topics/backend-events-topic
+export SUBSCRIPTION_ID=projects/$PROJECT_ID/subscriptions/backend-events-topic-sub
+export GCS_PATH=gs://gymshark-retail-events/output
+
+export EVENT_PATH=data/order_event.json
+
+#gcloud config set project $PROJECT_ID
+#gcloud auth application-default set-quota-project $PROJECT_ID
+#gcloud auth application-default login
+
+python3 publish.py --input=$EVENT_PATH --topic=$TOPIC_ID
+python3 pipeline.py --input=$SUBSCRIPTION_ID --streaming
