b9c196c596dcf0ed15e082280d9501c0e1614b06
Virgilio Gonzenbach <62161049+vgonzenbach@users.noreply.github.com>
2025-11-27 19:19:26 +0100

    init pipeline

---
+++ pipeline.py
@@ -0,0 +1,46 @@
+import argparse
+import logging
+import apache_beam as beam
+from apache_beam.options.pipeline_options import PipelineOptions, SetupOptions
+from apache_beam.runners.runner import PipelineResult
+
+from apache_beam.io import ReadFromPubSub, WriteToText
+
+
+class PrintElement(beam.DoFn):
+    def process(self, element):
+        print(element)
+        yield element
+
+
+def run(argv=None, save_main_session=True) -> PipelineResult:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--input',
+        dest='input',
+        help='Input subscription to pull from.')
+    parser.add_argument(
+        '--output',
+        dest='output',
+        required=False, # TODO: change
+        help='Output file to write results to.')
+    known_args, pipeline_args = parser.parse_known_args(argv)
+
+    pipeline_options = PipelineOptions(pipeline_args)
+
+    pipeline = beam.Pipeline(options=PipelineOptions(pipeline_args))
+    # check topic or subscription
+    message = (
+        pipeline 
+        | 'Read' >> ReadFromPubSub(subscription=known_args.input)
+        | 'Decode' >> beam.Map(lambda b: b.decode('utf-8'))
+        | 'PrintToStdout' >> beam.ParDo(PrintElement())
+    )
+
+    result = pipeline.run()
+    result.wait_until_finish()
+    return result
+
+if __name__ == '__main__':
+    logging.getLogger().setLevel(logging.INFO)
+    run()
